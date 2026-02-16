6edd4e43c048af4abdea5e2d01fbf25d564e839c
Ehsan Fani <ronaldo_esi@yahoo.com>
2026-02-16 15:51:56 +0100

    Added dummy user 1

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product')
+const User = require('./models/user')
 
 const app = express();
 
@@ -16,15 +18,35 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user
+            next()
+        }).catch(err => console.log(err))
 
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
+User.hasMany(Product)
+
+// sequelize.sync({force:true})// by changing in database structure it drops & creates automatically
 sequelize.sync()
     .then(
         result => {
-            app.listen(3000);
+            return User.findByPk(1)
+        })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Kalal', email: 'test@test.com' })
         }
-    ).catch(err => console.log(err))
+    }).then(user => {
+        console.log('user')
+        console.log(user)
+        app.listen(3000);
+    })
+    .catch(err => console.log(err))
